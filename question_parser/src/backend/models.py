30310cf18f93867d73d63141447a320ab23d7ca0
Artemidividiy <artemiy.kasyanik@gmail.com>
2023-10-22 19:57:34 +0300

    prepared alembic migration

--- question_parser/src/backend/models.py
+++ question_parser/src/backend/models.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from services.db_service import Base
 
+
 class Question(Base):
-  __tablename__ = "questions"
-  id = Column("id", Integer, primary_key=True, index=True)
-  question_text = Column("question_text", String, nullable=False, unique=True)
-  answer_text = Column("answer_text", String, nullable=False, unique=True)
-  question_created_at = Column("question_created_at", DateTime, nullable = False, unique = True)
-  
+    __tablename__ = "questions"
+    id = Column("id", Integer, primary_key=True, index=True)
+    question_text = Column("question_text", String,
+                           nullable=False, unique=True)
+    answer_text = Column("answer_text", String, nullable=False, unique=True)
+    question_created_at = Column(
+        "question_created_at", DateTime, nullable=False, unique=True)
